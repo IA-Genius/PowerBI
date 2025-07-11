c9054a729ce8de49c49d45666d4604adf43ec936
Cristtofer <cristofferchoque@gmail.com>
2025-07-11 09:32:20 -0500

    Panel de control - Power BI

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,35 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+
+                        <div id="iframe-container" class="iframe-container">
+                            <!--sandbox="allow-scripts"-->
+                            <iframe width="100%" height="700px" sandbox="allow-scripts allow-same-origin" id="launcher_reporte" src="https://app.powerbi.com/view?r=eyJrIjoiMmIyZmQ5MzEtMDIxOS00NjgxLTliOWMtMWQ1ZTI5YjExZThhIiwidCI6ImFjMGFmY2JlLTEzYzAtNDIyYi1iZGJhLTA0ZDk5ZmJlMzljZCIsImMiOjR9" frameborder="0" allowfullscreen="true"></iframe>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+Route::get('/gestionar-reportes', function () {
+    return Inertia::render('GestionarReportes');
+})->name('gestionarReportes');
+
+Route::get('/gestionar-usuarios', function () {
+    return Inertia::render('GestionarUsuarios');
+})->name('gestionarUsuarios');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
