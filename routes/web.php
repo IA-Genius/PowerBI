d4e10942387f1fd98366d3380c2310c3ee98fbba
nandoxps <luisfr986@gmail.com>
2025-07-16 10:46:09 -0500

    ajustes

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ require __DIR__ . '/auth.php';
 // PERFIL (AUTENTICADO)
 // ===================
 Route::middleware('auth')->group(function () {
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
